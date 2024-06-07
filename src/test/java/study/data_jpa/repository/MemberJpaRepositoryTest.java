88f9dd3373f28349e2077110b7c5e59f5c48862f
memoryDev <memorydev2024@gmail.com>
2024-06-07 16:21:06 +0900

    Initial commit

---
+++ src/test/java/study/data_jpa/repository/MemberJpaRepositoryTest.java
@@ -0,0 +1,32 @@
+package study.data_jpa.repository;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.transaction.annotation.Transactional;
+import study.data_jpa.entity.Member;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+@Transactional
+@Rollback(value = false)
+class MemberJpaRepositoryTest {
+
+    @Autowired
+    private MemberJpaRepository memberJpaRepository;
+
+    @Test
+    void testMember() {
+        Member member = new Member("memberA");
+        Member savedMember = memberJpaRepository.save(member);
+
+        Member findMember = memberJpaRepository.find(savedMember.getId());
+
+        assertThat(findMember.getId()).isEqualTo(savedMember.getId());
+        assertThat(findMember.getUsername()).isEqualTo(savedMember.getUsername());
+        assertThat(findMember).isEqualTo(savedMember);
+    }
+
+}
